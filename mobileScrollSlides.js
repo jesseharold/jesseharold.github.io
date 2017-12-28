3a8eccc5887e04b8518b0a377f9fc84cd6b20534
Jesse Harold <jesse.harold@leafgroup.com>
2017-12-28 14:11:13 -0800

    basic click to slide working

--- mobileScrollSlides.js
+++ mobileScrollSlides.js
@@ -1,3 +1,16 @@
 $(function(){
-  console.log("ready");
+  //console.log("ready");
+  $("#mobile-scroll-slides").children().append("<div class='mss-arrow'>VVV</div>").on("click", function(){
+    changeSlide($(this));
+  });
 });
+
+function changeSlide(oldSlide) {
+  //console.log("changeSlide", oldSlide);
+  oldSlide.removeClass("mss-active");
+  if (oldSlide.next().length){
+    oldSlide.next().addClass("mss-active");
+  } else {
+    oldSlide.siblings().eq(0).addClass("mss-active");
+  }
+}
